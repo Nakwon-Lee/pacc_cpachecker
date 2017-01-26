12178f79310c45d395872c123003475ee6fe602c
skrdnjs <skrdnjs0904@naver.com>
2017-01-26 20:54:13 +0900

    170126 20:53 add shell script for existing technique

---
+++ for_bench_exist.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+python3 ./scripts/benchexec --no-compress-results ./doc/examples/benchmark_remo_CSRPO_exist.xml
+
+#./scripts/benchmark.py ./doc/examples/benchmark_remo_CSBFS.xml
+
+#./scripts/benchmark.py ./doc/examples/benchmark_remo_CSDFS.xml
+
+#./scripts/benchmark.py ./doc/examples/benchmark_remo_CSRPO.xml
+
+#./scripts/benchmark.py ./doc/examples/benchmark_remo_BlkCSBFS.xml
+
+#./scripts/benchmark.py ./doc/examples/benchmark_remo_BlkCSDFS.xml
+
+#./scripts/benchmark.py ./doc/examples/benchmark_remo_BlkCSRPO.xml
+
+#./scripts/benchmark.py ./doc/examples/benchmark_remo_BlkBFS.xml
+
+#./scripts/benchmark.py ./doc/examples/benchmark_remo_BlkDFS.xml
+
+#./scripts/benchmark.py ./doc/examples/benchmark_remo_BlkRPO.xml
+
+#./scripts/benchmark.py ./doc/examples/benchmark_remo_BFS.xml
+
+#./scripts/benchmark.py ./doc/examples/benchmark_remo_DFS.xml
+
+#./scripts/benchmark.py ./doc/examples/benchmark_remo_RPO.xml
