7af6545e71a6bf061113f3805a30d12a047e48ea
skrdnjs <skrdnjs0904@naver.com>
2017-11-03 16:45:51 +0900

    171102 16:46 backup commit

--- FitSearchScript.sh
+++ FitSearchScript.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-TARGET_FILE="../sv-benchmarks/c/ntdrivers/floppy_true-unreach-call_true-valid-memsafety.i.cil.c"
+TARGET_FILE="../sv-benchmarks/c/eca-rers2012/Problem04_label00_true-unreach-call.c"
 
 for i in {1..30}
 do
-	sudo python3 ./scripts/RanTSExecutor.py --cores 0 --memlimit 8000000000 --no-container -- scripts/cpa.sh -heap 7000M -timelimit 30s -noout -Dy-MySearchStrategy-PredAbs-ABElf -preprocess -stats -setprop cpa.predicate.memoryAllocationsAlwaysSucceed=true -spec ../sv-benchmarks/c/ReachSafety.prp $TARGET_FILE
+	sudo python3 ./scripts/RanTSExecutor.py --cores 0 --memlimit 16000000000 --no-container -- scripts/cpa.sh -heap 14000M -timelimit 30s -noout -Dy-MySearchStrategy-PredAbs-ABElf -preprocess -stats -setprop cpa.predicate.memoryAllocationsAlwaysSucceed=true -spec ../sv-benchmarks/c/ReachSafety.prp $TARGET_FILE
 
-	sudo python3 ./scripts/TSExecutorFull.py --cores 0 --memlimit 8000000000 --no-container -- scripts/cpa.sh -heap 7000M -timelimit 900s -noout -Dy-MySearchStrategy-PredAbs-ABElf -preprocess -stats -setprop cpa.predicate.memoryAllocationsAlwaysSucceed=true -spec ../sv-benchmarks/c/ReachSafety.prp $TARGET_FILE
+	sudo python3 ./scripts/TSExecutorFull.py --cores 0 --memlimit 16000000000 --no-container -- scripts/cpa.sh -heap 14000M -timelimit 900s -noout -Dy-MySearchStrategy-PredAbs-ABElf -preprocess -stats -setprop cpa.predicate.memoryAllocationsAlwaysSucceed=true -spec ../sv-benchmarks/c/ReachSafety.prp $TARGET_FILE
 done
--- TSSearchScript.sh
+++ TSSearchScript.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo python3 ./scripts/TSSearch.py --cores 0 --memlimit 8000000000 --no-container -- scripts/cpa.sh -heap 7000M -timelimit 30s -noout -Dy-MySearchStrategy-PredAbs-ABElf -preprocess -stats -setprop cpa.predicate.memoryAllocationsAlwaysSucceed=true -spec ../sv-benchmarks/c/ReachSafety.prp ../sv-benchmarks/c/ntdrivers/floppy2_true-unreach-call.i.cil.c
+TARGET_FILE="../sv-benchmarks/c/ssh/s3_srvr.blast.01_true-unreach-call.i.cil.c"
+
+sudo python3 ./scripts/TSSearch.py --cores 0 --memlimit 16000000000 --no-container -- scripts/cpa.sh -heap 14000M -timelimit 30s -noout -Dy-MySearchStrategy-PredAbs-ABElf -preprocess -stats -setprop cpa.predicate.memoryAllocationsAlwaysSucceed=true -spec ../sv-benchmarks/c/ReachSafety.prp $TARGET_FILE
