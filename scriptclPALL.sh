93797421711badf01d390d532848046134386de3
skrdnjs <skrdnjs0904@naver.com>
2018-04-18 12:54:22 +0900

    180418 12:54 make script for runs

---
+++ scriptclPALL.sh
@@ -0,0 +1,53 @@
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/PL-minp-$1.csv cl$1PALL0.csv cl$1PALL0/ -PredAbs-ABEl
+
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/array-ex-$1.csv cl$1PALL1.csv cl$1PALL1/ -PredAbs-ABEl
+
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/array-ind-pat-$1.csv cl$1PALL2.csv cl$1PALL2/ -PredAbs-ABEl
+
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/array-til-$1.csv cl$1PALL3.csv cl$1PALL3/ -PredAbs-ABEl
+
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/ddv-mach-$1.csv cl$1PALL4.csv cl$1PALL4/ -PredAbs-ABEl
+
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/for-heap-$1.csv cl$1PALL5.csv cl$1PALL5/ -PredAbs-ABEl
+
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/ldv-com-te-$1.csv cl$1PALL6.csv cl$1PALL6/ -PredAbs-ABEl
+
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/ldv-consum-29to113-$1.csv cl$1PALL7.csv cl$1PALL7/ -PredAbs-ABEl
+
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/ldv-li-3.0-$1.csv cl$1PALL8.csv cl$1PALL8/ -PredAbs-ABEl
+
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/ldv-li-4.0-$1.csv cl$1PALL9.csv cl$1PALL9/ -PredAbs-ABEl
+
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/ldv-reg-$1.csv cl$1PALL10.csv cl$1PALL10/ -PredAbs-ABEl
+
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/ldv-reg-test-$1.csv cl$1PALL11.csv cl$1PALL11/ -PredAbs-ABEl
+
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/loop-acc-$1.csv cl$1PALL12.csv cl$1PALL12/ -PredAbs-ABEl
+
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/loop-inv-$1.csv cl$1PALL13.csv cl$1PALL13/ -PredAbs-ABEl
+
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/loop-lit-$1.csv cl$1PALL14.csv cl$1PALL14/ -PredAbs-ABEl
+
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/loops-$1.csv cl$1PALL15.csv cl$1PALL15/ -PredAbs-ABEl
+
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/reducer-$1.csv cl$1PALL16.csv cl$1PALL16/ -PredAbs-ABEl
+
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/rers-P01-$1.csv cl$1PALL17.csv cl$1PALL17/ -PredAbs-ABEl
+
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/rers-P02-$1.csv cl$1PALL18.csv cl$1PALL18/ -PredAbs-ABEl
+
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/rers-P03-$1.csv cl$1PALL19.csv cl$1PALL19/ -PredAbs-ABEl
+
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/rers-P04-$1.csv cl$1PALL20.csv cl$1PALL20/ -PredAbs-ABEl
+
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/seq-mth-15to65-$1.csv cl$1PALL21.csv cl$1PALL21/ -PredAbs-ABEl
+
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/seq-pth-$1.csv cl$1PALL22.csv cl$1PALL22/ -PredAbs-ABEl
+
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/ssh-$1.csv cl$1PALL23.csv cl$1PALL23/ -PredAbs-ABEl
+
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/ssh-sim-$1.csv cl$1PALL24.csv cl$1PALL24/ -PredAbs-ABEl
+
+python3 scripts/FitSearchExecutor.py GIVEN2 filelistcsvs/needtoexec/real/systemc-$1.csv cl$1PALL25.csv cl$1PALL25/ -PredAbs-ABEl
+
+
