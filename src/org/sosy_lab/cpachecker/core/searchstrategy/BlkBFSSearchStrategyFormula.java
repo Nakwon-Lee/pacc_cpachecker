bd481635316d43f48549e04cc8aaa0033f30c728
skrdnjs <skrdnjs0904@naver.com>
2018-10-21 22:27:32 +0900

    181021 add blkdfs and bfs

---
+++ src/org/sosy_lab/cpachecker/core/searchstrategy/BlkBFSSearchStrategyFormula.java
@@ -0,0 +1,80 @@
+/*
+ *  CPAchecker is a tool for configurable software verification.
+ *  This file is part of CPAchecker.
+ *
+ *  Copyright (C) 2007-2018  Dirk Beyer
+ *  All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.sosy_lab.cpachecker.core.searchstrategy;
+
+import org.sosy_lab.cpachecker.core.interfaces.SearchStrategyFormula;
+import org.sosy_lab.cpachecker.cpa.arg.ARGState;
+
+public class BlkBFSSearchStrategyFormula implements SearchStrategyFormula {
+
+  public BlkBFSSearchStrategyFormula() {
+    super();
+  }
+
+  @Override
+  public int compare(ARGState e1, ARGState e2) {
+    // compare start
+    if (e1.isAbs() < 1 && e2.isAbs() >= 1) {
+      return 1;
+    } else if (e1.isAbs() >= 1 && e2.isAbs() < 1) {
+      return -1;
+    } else {
+      int thePhi = e1.isAbs();
+      if (thePhi < 1) {
+        if (e1.CS() < e2.CS()) {
+          return -1;
+        } else if (e1.CS() > e2.CS()) {
+          return 1;
+        } else {
+          if (e1.RPO() < e2.RPO()) {
+            return -1;
+          } else if (e1.RPO() > e2.RPO()) {
+            return 1;
+          } else {
+            return 0;
+          }
+        }
+      } else {
+        if (e1.blkD() < e2.blkD()) {
+          return 1;
+        } else if (e1.blkD() > e2.blkD()) {
+          return -1;
+        } else {
+          if (e1.uID() < e2.uID()) {
+            return -1;
+          } else if (e1.uID() > e2.uID()) {
+            return 1;
+          } else {
+            return 0;
+          }
+        }
+      }
+    }
+    // compare end
+  }
+
+  public static class Factory implements SearchStrategyFormula.Factory {
+
+    @Override
+    public SearchStrategyFormula create() {
+      return new BlkBFSSearchStrategyFormula();
+    }
+  }
+}
