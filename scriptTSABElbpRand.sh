e2fe11d94690d369a1a2af83223e9422f632f61e
skrdnjs <skrdnjs0904@naver.com>
2018-11-17 00:36:13 +0900

    181117 fix script

--- scriptTSABElbpRand.sh
+++ scriptTSABElbpRand.sh
@@ -4,5 +4,5 @@ for i in {0..9}
 do
   sudo swapoff -a
   python3 WhatMachineR.py doc/examples/benchmark-TS-ABElbp-RanTS-machine.xml $1 $i
-  sudo nohup ./scripts/benchexec --no-container doc/examples/benchmark-TS-ABElbp-RanTS-machine.xml
+  sudo ./scripts/benchexec --no-container doc/examples/benchmark-TS-ABElbp-RanTS-machine.xml
 done
