a15df4ab6260794e5ea090ee55471fa6e60442eb
skrdnjs <skrdnjs0904@naver.com>
2018-12-22 01:42:18 +0900

    181222 fix seqp greedy and add PAV1-G

--- dupmod.sh
+++ dupmod.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-mv m$3/benchmark-TS-CPAseqp-CE2-machine.2018-$1.results.Test-CPAseqp-CE2.txt m$3/benchmark-TS-CPAseqp-CE2-machine.2018-$2.results.Test-CPAseqp-CE2.txt
+mv m$3/benchmark-TS-CPAseqp-CE3-machine.2018-$1.results.Test-CPAseqp-CE3.txt m$3/benchmark-TS-CPAseqp-CE3-machine.2018-$2.results.Test-CPAseqp-CE3.txt
 
-mv m$3/benchmark-TS-CPAseqp-CE2-machine.2018-$1.results.Test-CPAseqp-CE2.xml.bz2 m$3/benchmark-TS-CPAseqp-CE2-machine.2018-$2.results.Test-CPAseqp-CE2.xml.bz2
+mv m$3/benchmark-TS-CPAseqp-CE3-machine.2018-$1.results.Test-CPAseqp-CE3.xml.bz2 m$3/benchmark-TS-CPAseqp-CE3-machine.2018-$2.results.Test-CPAseqp-CE3.xml.bz2
 
-mv m$3/benchmark-TS-CPAseqp-CE2-machine.2018-$1.results.Test-CPAseqp-CE2.MachineFiles-$3.xml.bz2 m$3/benchmark-TS-CPAseqp-CE2-machine.2018-$2.results.Test-CPAseqp-CE2.MachineFiles-$3.xml.bz2
+mv m$3/benchmark-TS-CPAseqp-CE3-machine.2018-$1.results.Test-CPAseqp-CE3.MachineFiles-$3.xml.bz2 m$3/benchmark-TS-CPAseqp-CE3-machine.2018-$2.results.Test-CPAseqp-CE3.MachineFiles-$3.xml.bz2
 
-mv m$3/benchmark-TS-CPAseqp-CE2-machine.2018-$1.results.Test-CPAseqp-CE2.MachineFiles64-$3.xml.bz2 m$3/benchmark-TS-CPAseqp-CE2-machine.2018-$2.results.Test-CPAseqp-CE2.MachineFiles64-$3.xml.bz2
+mv m$3/benchmark-TS-CPAseqp-CE3-machine.2018-$1.results.Test-CPAseqp-CE3.MachineFiles64-$3.xml.bz2 m$3/benchmark-TS-CPAseqp-CE3-machine.2018-$2.results.Test-CPAseqp-CE3.MachineFiles64-$3.xml.bz2
 
-unzip m$3/benchmark-TS-CPAseqp-CE2-machine.2018-$1.logfiles.zip
+unzip m$3/benchmark-TS-CPAseqp-CE3-machine.2018-$1.logfiles.zip
 
-mv benchmark-TS-CPAseqp-CE2-machine.2018-$1.logfiles/ benchmark-TS-CPAseqp-CE2-machine.2018-$2.logfiles/
+mv benchmark-TS-CPAseqp-CE3-machine.2018-$1.logfiles/ benchmark-TS-CPAseqp-CE3-machine.2018-$2.logfiles/
 
-zip benchmark-TS-CPAseqp-CE2-machine.2018-$2.logfiles.zip benchmark-TS-CPAseqp-CE2-machine.2018-$2.logfiles/*
+zip benchmark-TS-CPAseqp-CE3-machine.2018-$2.logfiles.zip benchmark-TS-CPAseqp-CE3-machine.2018-$2.logfiles/*
 
-mv benchmark-TS-CPAseqp-CE2-machine.2018-$2.logfiles.zip m$3/
+mv benchmark-TS-CPAseqp-CE3-machine.2018-$2.logfiles.zip m$3/
--- scriptTSCPAseqes.sh
+++ scriptTSCPAseqes.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 sudo swapoff -a
-python3 WhatMachine.py doc/examples/benchmark-TS-CPAseqp-CE3-machine.xml $1
-sudo ./scripts/benchexec --no-container doc/examples/benchmark-TS-CPAseqp-CE3-machine.xml
+python3 WhatMachine.py doc/examples/benchmark-TS-ABEl-CPAseqp-machine.xml $1
+sudo ./scripts/benchexec --no-container doc/examples/benchmark-TS-ABEl-CPAseqp-machine.xml
+sftp -b kresultup spiralftp@spiral.kaist.ac.kr
+
+sudo swapoff -a
+python3 WhatMachine.py doc/examples/benchmark-TS-CPAseqp-PAV1-G-machine.xml $1
+sudo ./scripts/benchexec --no-container doc/examples/benchmark-TS-CPAseqp-PAV1-G-machine.xml
+sftp -b kresultup spiralftp@spiral.kaist.ac.kr
+
+sudo swapoff -a
+python3 WhatMachine.py doc/examples/benchmark-TS-CPAseqp-CE2-machine.xml $1
+sudo ./scripts/benchexec --no-container doc/examples/benchmark-TS-CPAseqp-CE2-machine.xml
 sftp -b kresultup spiralftp@spiral.kaist.ac.kr
