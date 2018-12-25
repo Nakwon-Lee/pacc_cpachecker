c373930173c7c55ea5c4ab0a4bd3ee358c5471ab
skrdnjs <skrdnjs0904@naver.com>
2018-12-25 17:30:07 +0900

    181225 fix files for seqes rand

--- dupmodsudo.sh
+++ dupmodsudo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rm -R benchmark-TS-CPAseqp-CE3-machine.2018-$2.logfiles/
+rm -R benchmark-TS-CPAseqp-LoopStackSorted-machine.2018-$2.logfiles/
 
-rm m$3/benchmark-TS-CPAseqp-CE3-machine.2018-$1.logfiles.zip
+rm m$3/benchmark-TS-CPAseqp-LoopStackSorted-machine.2018-$1.logfiles.zip
