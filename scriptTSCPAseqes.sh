031858e9183e1980693398cd84a4c65ab0abb7c6
skrdnjs <skrdnjs0904@naver.com>
2018-12-25 17:46:49 +0900

    181225 fix script for seq

--- scriptTSCPAseqes.sh
+++ scriptTSCPAseqes.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-sudo swapoff -a
-python3 WhatMachine.py doc/examples/benchmark-TS-CPAseqp-LoopStackSorted-machine.xml $1
-sudo ./scripts/benchexec --no-container doc/examples/benchmark-TS-CPAseqp-LoopStackSorted-machine.xml
-sftp -b kresultup spiralftp@spiral.kaist.ac.kr
-
 for i in {0..9}
 do
   sudo swapoff -a
