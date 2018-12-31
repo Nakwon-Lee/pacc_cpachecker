96bbc875fa8fde150eaa0a7dff88f66dc8d15ede
skrdnjs <skrdnjs0904@naver.com>
2018-12-31 20:40:16 +0900

    181231 fix AGR

--- scriptTSCPAseqes.sh
+++ scriptTSCPAseqes.sh
@@ -4,6 +4,6 @@ for i in {0..9}
 do
   sudo swapoff -a
   python3 WhatMachine.py doc/examples/AGR/benchmark-TS-CPAseqp-AGR$i-machine.xml $1
-  sudo ./scripts/benchexec --no-container doc/examples/valuerands/benchmark-TS-CPAseqp-AGR$i-machine.xml
+  sudo ./scripts/benchexec --no-container doc/examples/AGR/benchmark-TS-CPAseqp-AGR$i-machine.xml
   sftp -b kresultup spiralftp@spiral.kaist.ac.kr
 done
