c6848da686ba349971ef09460b0df5afbd1a3447
skrdnjs <skrdnjs0904@naver.com>
2019-02-01 19:59:32 +0900

    190201 commit

--- dupmod.sh
+++ dupmod.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-mv m$3/benchmark-TS-CPAseqp-Rand3-machine.2018-$1.results.Test-CPAseqp-Rand3.txt m$3/benchmark-TS-CPAseqp-Rand3-machine.2018-$2.results.Test-CPAseqp-Rand3.txt
+mv m$3/benchmark-TS-CPAseqp-AGR9-machine.$1.results.Test-CPAseqp-AGR9.txt m$3/benchmark-TS-CPAseqp-AGR9-machine.$2.results.Test-CPAseqp-AGR9.txt
 
-mv m$3/benchmark-TS-CPAseqp-Rand3-machine.2018-$1.results.Test-CPAseqp-Rand3.xml.bz2 m$3/benchmark-TS-CPAseqp-Rand3-machine.2018-$2.results.Test-CPAseqp-Rand3.xml.bz2
+mv m$3/benchmark-TS-CPAseqp-AGR9-machine.$1.results.Test-CPAseqp-AGR9.xml.bz2 m$3/benchmark-TS-CPAseqp-AGR9-machine.$2.results.Test-CPAseqp-AGR9.xml.bz2
 
-mv m$3/benchmark-TS-CPAseqp-Rand3-machine.2018-$1.results.Test-CPAseqp-Rand3.MachineFiles-$3.xml.bz2 m$3/benchmark-TS-CPAseqp-Rand3-machine.2018-$2.results.Test-CPAseqp-Rand3.MachineFiles-$3.xml.bz2
+mv m$3/benchmark-TS-CPAseqp-AGR9-machine.$1.results.Test-CPAseqp-AGR9.MachineFiles-$3.xml.bz2 m$3/benchmark-TS-CPAseqp-AGR9-machine.$2.results.Test-CPAseqp-AGR9.MachineFiles-$3.xml.bz2
 
-mv m$3/benchmark-TS-CPAseqp-Rand3-machine.2018-$1.results.Test-CPAseqp-Rand3.MachineFiles64-$3.xml.bz2 m$3/benchmark-TS-CPAseqp-Rand3-machine.2018-$2.results.Test-CPAseqp-Rand3.MachineFiles64-$3.xml.bz2
+mv m$3/benchmark-TS-CPAseqp-AGR9-machine.$1.results.Test-CPAseqp-AGR9.MachineFiles64-$3.xml.bz2 m$3/benchmark-TS-CPAseqp-AGR9-machine.$2.results.Test-CPAseqp-AGR9.MachineFiles64-$3.xml.bz2
 
-unzip m$3/benchmark-TS-CPAseqp-Rand3-machine.2018-$1.logfiles.zip
+unzip m$3/benchmark-TS-CPAseqp-AGR9-machine.$1.logfiles.zip
 
-mv benchmark-TS-CPAseqp-Rand3-machine.2018-$1.logfiles/ benchmark-TS-CPAseqp-Rand3-machine.2018-$2.logfiles/
+mv benchmark-TS-CPAseqp-AGR9-machine.$1.logfiles/ benchmark-TS-CPAseqp-AGR9-machine.$2.logfiles/
 
-zip benchmark-TS-CPAseqp-Rand3-machine.2018-$2.logfiles.zip benchmark-TS-CPAseqp-Rand3-machine.2018-$2.logfiles/*
+zip benchmark-TS-CPAseqp-AGR9-machine.$2.logfiles.zip benchmark-TS-CPAseqp-AGR9-machine.$2.logfiles/*
 
-mv benchmark-TS-CPAseqp-Rand3-machine.2018-$2.logfiles.zip m$3/
+mv benchmark-TS-CPAseqp-AGR9-machine.$2.logfiles.zip m$3/
--- dupmodsudo.sh
+++ dupmodsudo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rm -R benchmark-TS-CPAseqp-Rand3-machine.2018-$2.logfiles/
+rm -R benchmark-TS-CPAseqp-AGR9-machine.$2.logfiles/
 
-rm m$3/benchmark-TS-CPAseqp-Rand3-machine.2018-$1.logfiles.zip
+rm m$3/benchmark-TS-CPAseqp-AGR9-machine.$1.logfiles.zip
