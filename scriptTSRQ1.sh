57e47d1267a2106b4e7f2290242cc7e0ee9e280c
skrdnjs <skrdnjs0904@naver.com>
2019-02-01 20:11:31 +0900

    180201 RQ1 repeat

--- scriptTSRQ1.sh
+++ scriptTSRQ1.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
-sudo swapoff -a
-python3 WhatMachine.py doc/examples/benchmark-TS-ABEl-Exist1-machine.xml $1
-sudo ./scripts/benchexec --no-container doc/examples/benchmark-TS-ABEl-Exist1-machine.xml
-sftp -b kresultup spiralftp@spiral.kaist.ac.kr
+for i in {0..5}
+do
+	sudo swapoff -a
+	python3 WhatMachine.py doc/examples/benchmark-TS-ABEl-Exist1-machine.xml $1
+	sudo ./scripts/benchexec --no-container doc/examples/benchmark-TS-ABEl-Exist1-machine.xml
+	sftp -b kresultup spiralftp@spiral.kaist.ac.kr
+done
 
-sudo swapoff -a
-python3 WhatMachine.py doc/examples/benchmark-TS-ABEl-BlkBFS-machine.xml $1
-sudo ./scripts/benchexec --no-container doc/examples/benchmark-TS-ABEl-BlkBFS-machine.xml
-sftp -b kresultup spiralftp@spiral.kaist.ac.kr
-
-sudo swapoff -a
-python3 WhatMachine.py doc/examples/benchmark-TS-ABEl-BlkDFS-machine.xml $1
-sudo ./scripts/benchexec --no-container doc/examples/benchmark-TS-ABEl-BlkDFS-machine.xml
-sftp -b kresultup spiralftp@spiral.kaist.ac.kr
-
-sudo swapoff -a
-python3 WhatMachine.py doc/examples/benchmark-TS-ABEl-AvoidChainEffect-machine.xml $1
-sudo ./scripts/benchexec --no-container doc/examples/benchmark-TS-ABEl-AvoidChainEffect-machine.xml
-sftp -b kresultup spiralftp@spiral.kaist.ac.kr
-
-sudo swapoff -a
-python3 WhatMachine.py doc/examples/benchmark-TS-ABElbp-AvoidChainEffect-machine.xml $1
-sudo ./scripts/benchexec --no-container doc/examples/benchmark-TS-ABElbp-AvoidChainEffect-machine.xml
-sftp -b kresultup spiralftp@spiral.kaist.ac.kr
+for i in {0..5}
+do
+	sudo swapoff -a
+	python3 WhatMachine.py doc/examples/benchmark-TS-ABEl-AvoidChainEffect-machine.xml $1
+	sudo ./scripts/benchexec --no-container doc/examples/benchmark-TS-ABEl-AvoidChainEffect-machine.xml
+	sftp -b kresultup spiralftp@spiral.kaist.ac.kr
+done
