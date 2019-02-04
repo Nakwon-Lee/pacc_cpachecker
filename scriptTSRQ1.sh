d1c5d3417b4db1f2d2967d5c2cbe46c2aaf5baf6
skrdnjs <skrdnjs0904@naver.com>
2019-02-04 20:52:51 +0900

    180204 modi kresultup and script RQ1

--- scriptTSRQ1.sh
+++ scriptTSRQ1.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-for i in {0..5}
+for i in {1..5}
 do
 	sudo swapoff -a
 	python3 WhatMachine.py doc/examples/benchmark-TS-ABEl-Exist1-machine.xml $1
 	sudo ./scripts/benchexec --no-container doc/examples/benchmark-TS-ABEl-Exist1-machine.xml
 	sftp -b kresultup spiralftp@spiral.kaist.ac.kr
-done
-
-for i in {0..5}
-do
-	sudo swapoff -a
+        sudo swapoff -a
 	python3 WhatMachine.py doc/examples/benchmark-TS-ABEl-AvoidChainEffect-machine.xml $1
 	sudo ./scripts/benchexec --no-container doc/examples/benchmark-TS-ABEl-AvoidChainEffect-machine.xml
 	sftp -b kresultup spiralftp@spiral.kaist.ac.kr
