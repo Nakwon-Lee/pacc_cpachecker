1a989c4bfd0a2e5c3d5aea44d86e734b5bcd449b
skrdnjs <skrdnjs0904@naver.com>
2019-02-11 23:38:23 +0900

    190211 final (hopely) exp.

--- scriptTSRQ1.sh
+++ scriptTSRQ1.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-for i in {1..5}
-do
-	sudo swapoff -a
-	python3 WhatMachine.py doc/examples/benchmark-TS-ABEl-Exist1-machine.xml $1
-	sudo ./scripts/benchexec --no-container doc/examples/benchmark-TS-ABEl-Exist1-machine.xml
-	sftp -b kresultup spiralftp@spiral.kaist.ac.kr
-        sudo swapoff -a
-	python3 WhatMachine.py doc/examples/benchmark-TS-ABEl-AvoidChainEffect-machine.xml $1
-	sudo ./scripts/benchexec --no-container doc/examples/benchmark-TS-ABEl-AvoidChainEffect-machine.xml
-	sftp -b kresultup spiralftp@spiral.kaist.ac.kr
-done
+sudo swapoff -a
+python3 WhatMachine.py doc/examples/benchmark-TS-ABEl-Exist1-machine.xml $1
+sudo ./scripts/benchexec --no-container doc/examples/benchmark-TS-ABEl-Exist1-machine.xml
+sftp -b kresultup spiralftp@spiral.kaist.ac.kr
+sudo swapoff -a
+python3 WhatMachine.py doc/examples/benchmark-TS-ABEl-AvoidChainEffect-machine.xml $1
+sudo ./scripts/benchexec --no-container doc/examples/benchmark-TS-ABEl-AvoidChainEffect-machine.xml
+sftp -b kresultup spiralftp@spiral.kaist.ac.kr
