debd6da45b74c3344ca7165f5a4c1a8044ed8dfb
skrdnjs <skrdnjs0904@naver.com>
2019-02-20 21:11:34 +0900

    190220 add Makekresultup

---
+++ Makekresultup.py
@@ -0,0 +1,25 @@
+import sys
+
+DIRECTORY_PATH = 'sftpbatches/'
+BATCH_STR_PRE = 'put results/benchmark* nwlee/RQ1-5/T'
+BATCH_STR_MID = '/m'
+BATCH_STR_SUF = '/\nquit\n'
+FILE_NAME_PRE = 'kresultup'
+
+'''
+usage: python Makekresultup.py <trial num> <machine num>
+'''
+
+def main():
+    args = sys.argv
+    batchname = DIRECTORY_PATH + FILE_NAME_PRE
+    batchfile = open(batchname,'w')
+
+    inst = BATCH_STR_PRE + args[1] + BATCH_STR_MID + args[2] + BATCH_STR_SUF
+
+    batchfile.write(inst)
+
+    batchfile.close()
+
+if __name__ == "__main__":
+    main()
