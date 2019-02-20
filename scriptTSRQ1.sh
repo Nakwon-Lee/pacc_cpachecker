9416b74e6a1b338e84b06c1195cef0a16d2218ab
skrdnjs <skrdnjs0904@naver.com>
2019-02-20 12:48:52 +0900

    190220 modi to 4core limits and script

--- scriptTSRQ1.sh
+++ scriptTSRQ1.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-for i in {1..4}
+for i in {1..5}
 do
+	python3 Makekresultup.py $i $1
 	sudo swapoff -a
 	python3 WhatMachine.py doc/examples/benchmark-TS-ABEl-Exist1-machine.xml $1
 	sudo ./scripts/benchexec --no-container doc/examples/benchmark-TS-ABEl-Exist1-machine.xml
-	sftp -b kresultup spiralftp@spiral.kaist.ac.kr
+	sftp -b sftpbatches/kresultup spiralftp@spiral.kaist.ac.kr
 	sudo swapoff -a
 	python3 WhatMachine.py doc/examples/benchmark-TS-ABEl-AvoidChainEffect-machine.xml $1
 	sudo ./scripts/benchexec --no-container doc/examples/benchmark-TS-ABEl-AvoidChainEffect-machine.xml
-	sftp -b kresultup spiralftp@spiral.kaist.ac.kr
+	sftp -b sftpbatches/kresultup spiralftp@spiral.kaist.ac.kr
 done
