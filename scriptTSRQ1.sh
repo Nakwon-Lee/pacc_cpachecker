87f9a0443cc182686e6975a252c5239adda0c110
skrdnjs <skrdnjs0904@naver.com>
2019-02-26 19:39:10 +0900

    190226 script 6 to 30, bkup directory

--- scriptTSRQ1.sh
+++ scriptTSRQ1.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-for i in {1..5}
+for i in {6..30}
 do
 	python3 Makekresultup.py $i $1
 	sudo swapoff -a
 	python3 WhatMachine.py doc/examples/benchmark-TS-ABEl-Exist1-machine.xml $1
 	sudo ./scripts/benchexec --no-container doc/examples/benchmark-TS-ABEl-Exist1-machine.xml
 	sftp -b sftpbatches/kresultup spiralftp@spiral.kaist.ac.kr
+	sudo cp results/benchmark* resultsbkup/
+	sudo rm results/benchmark*
 	sudo swapoff -a
 	python3 WhatMachine.py doc/examples/benchmark-TS-ABEl-AvoidChainEffect-machine.xml $1
 	sudo ./scripts/benchexec --no-container doc/examples/benchmark-TS-ABEl-AvoidChainEffect-machine.xml
 	sftp -b sftpbatches/kresultup spiralftp@spiral.kaist.ac.kr
+	sudo cp results/benchmark* resultsbkup/
+	sudo rm results/benchmark*
 done
