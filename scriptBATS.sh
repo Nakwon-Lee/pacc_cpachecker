35d9c36cbd03b568ce32c70ee6afd6cb2dd88350
skrdnjs <skrdnjs0904@naver.com>
2019-07-07 18:43:07 +0900

    190707 18:43 fix sh BATS

--- scriptBATS.sh
+++ scriptBATS.sh
@@ -5,27 +5,27 @@ python3 Makekresultup.py 0 $1
 sudo swapoff -a
 python3 WhatMachine.py doc/examples/benchmark-LPAABE-Ex.xml $1
 sudo benchexec --no-container doc/examples/benchmark-LPAABE-Ex.xml
-sftp -b sftpbatches/kresultup spiralftp@spiral.kaist.ac.kr
+sudo sftp -b sftpbatches/kresultup spiralftp@spiral.kaist.ac.kr
 sudo cp results/benchmark* resultsbkup/
 sudo rm results/benchmark*
 
 sudo swapoff -a
 python3 WhatMachine.py doc/examples/benchmark-LPAABE-BATS.xml $1
 sudo benchexec --no-container doc/examples/benchmark-LPAABE-BATS.xml
-sftp -b sftpbatches/kresultup spiralftp@spiral.kaist.ac.kr
+sudo sftp -b sftpbatches/kresultup spiralftp@spiral.kaist.ac.kr
 sudo cp results/benchmark* resultsbkup/
 sudo rm results/benchmark*
 
 sudo swapoff -a
 python3 WhatMachine.py doc/examples/benchmark-LPAABE-BAM-Ex.xml $1
 sudo benchexec --no-container doc/examples/benchmark-LPAABE-BAM-Ex.xml
-sftp -b sftpbatches/kresultup spiralftp@spiral.kaist.ac.kr
+sudo sftp -b sftpbatches/kresultup spiralftp@spiral.kaist.ac.kr
 sudo cp results/benchmark* resultsbkup/
 sudo rm results/benchmark*
 
 sudo swapoff -a
 python3 WhatMachine.py doc/examples/benchmark-LPAABE-BAM-BATS.xml $1
 sudo benchexec --no-container doc/examples/benchmark-LPAABE-BAM-BATS.xml
-sftp -b sftpbatches/kresultup spiralftp@spiral.kaist.ac.kr
+sudo sftp -b sftpbatches/kresultup spiralftp@spiral.kaist.ac.kr
 sudo cp results/benchmark* resultsbkup/
 sudo rm results/benchmark*
