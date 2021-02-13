54d4afa5e28119ab495c77b837db7e7f21b86de8
Nakwon-Lee <skrdnjs0904@naver.com>
2021-02-13 22:26:18 +0900

    210213 22:26 add scripts

---
+++ scriptT6-210213.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for i in {70..84}
+do
+	sudo swapoff -a
+	sudo benchexec cpa-bam-bnb-$i.xml
+	
+	sudo sftp -b kresultup spiralftp@spiral.kaist.ac.kr
+	sudo cp results/cpa-bam-bnb* resultsbkup/
+	sudo rm results/cpa-bam-bnb*
+done
---
+++ scriptT7-210213.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for i in {43..69}
+do
+	sudo benchexec cpa-bam-bnb-$i.xml
+	
+	sudo sftp -b kresultup spiralftp@spiral.kaist.ac.kr
+	sudo cp results/cpa-bam-bnb* resultsbkup/
+	sudo rm results/cpa-bam-bnb*
+done
