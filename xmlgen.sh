57854f2555e14bb7d21df1acf62916d0d18237fd
Nakwon-Lee <skrdnjs0904@naver.com>
2021-03-01 00:20:34 +0900

    210301 12:20 fix xmlgen

--- xmlgen.sh
+++ xmlgen.sh
@@ -2,6 +2,6 @@
 
 for i in {0..99}
 do
-	python3 xmlforbenchgen.py cpa-bam-bmc-X.xml $i cpa-bam-bmc-$i.xml
+	python3 xmlforbenchgen.py cpa-bam-dmc-X.xml $i cpa-bam-dmc-$i.xml
 	python3 xmlforbenchgen.py cpa-bam-bnb-X.xml $i cpa-bam-bnb-$i.xml
 done
